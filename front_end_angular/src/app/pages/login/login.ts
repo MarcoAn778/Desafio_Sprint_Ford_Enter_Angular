07d8b837e90e079e84b20d822eecdebb3b537597
Marco Antônio <marco.duoli2@gmail.com>
2026-06-16 15:55:27 -0300

    Adicionando db na api e login

--- front_end_angular/src/app/pages/login/login.ts
+++ front_end_angular/src/app/pages/login/login.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
-export class Login {}
+export class LoginComponent {
+  nome = '';
+  senha = '';
+  erro = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  entrar(): void {
+    this.erro = '';
+
+    this.authService.login(this.nome, this.senha).subscribe({
+      next: usuario => {
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+        this.router.navigate(['/home']);
+      },
+      error: erro => {
+        this.erro = erro.error?.message || 'Erro ao fazer login.';
+      }
+    });
+  }
+}
